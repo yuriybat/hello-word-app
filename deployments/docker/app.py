d5c7299d5daf3e704c82188d471457fcf4ca1289
yuriybat <yuriybat@yahoo.com>
2021-06-08 01:31:25 +0000

    added name

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'hello world',
+        'message': 'Hello I am a developer my name is Yuriy B',
         'environment': os.environ.get('ENVIRONMENT'),
         'owner': 'yuriybat',
         'namespace': os.environ.get('NAMESPACE')
     })
 
 
-@app.route('/soon')
+@app.route('/yuriybat')
 def comming_soon():
     return jsonify({
-        'message': 'This is comming soon page!!'
+        'message': 'This is Yuriy B page!!'
     })
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
